19b88ed35b1be6bee50d84799ce6f905e8ec556d
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2024-01-10 20:43:55 -0400

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React,{ useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+
+  const plus = (e) => {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value))
+  }
+  const minus = (e) => {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value))
+  }
+  const times = (e) => {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value))
+  }
+  const divide = (e) => {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value))
+  }
+
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  }
+
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input 
+        type='number'
+        ref={inputRef}
+        pattern='[0-9]'
+        placeholder='type a number'
+         />
+        <button onClick={plus}>+</button>
+        <button onClick={minus}>-</button>
+        <button onClick={times}>*</button>
+        <button onClick={divide}>/</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
+
+      </form>
     </div>
   );
 }
